a484ce85f12048f0d580d2cdcef2d3fce69eb5da
JRCSalter <jrcsalter@hotmail.co.uk>
2018-05-09 13:14:44 +0100

    Added login table/form for login.php

--- login.php
+++ login.php
@@ -13,6 +13,36 @@ $title = isset( $_GET[ "title" ] ) ? $_GET[ "title" ] : "Login";
 
 displayHeader( $meta, $title );
 
+?>
+
+<form action='post' method ='login.php' id='loginForm'>
+	<table>
+		<tr>
+			<td>
+				<label for='name'>Username:</label>
+			</td>
+      <td>
+        <input type='text' name='name'>
+      </td>
+		</tr>
+    <tr>
+      <td>
+        <label for='password'>Password: </label>
+      </td>
+      <td>
+        <input type='password' name='password'>
+      </td>
+    </tr>
+    <tr>
+      <td>
+        <input type='submit' name='submit'>
+      </td>
+    </tr>
+	</table>
+</form>
+
+<?php
+
 displayFooter();
 
 ?>
