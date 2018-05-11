37c4be97b3831c99bde61be062ab6ae0c031103f
JRCSalter <jrcsalter@hotmail.co.uk>
2018-05-11 22:48:18 +0100

    n/a

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 
 define( "DB_DSN",      "mysql:dbname=forum" );
 define( "DB_USERNAME", "root"               );
-define( "DB_PASSWORD", "DarthDoctor421001"  );
+define( "DB_PASSWORD", "Thisis@pw11"        );
 define( "PAGE_SIZE",   5                    );
 
 ?>
